65244270cbb727bf5ddcd6ddcbaafea4398b1d4c
smptxxx <fghzbnzjzjsbsbs@gmail.com>
2026-07-08 10:34:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 module.exports = {
     // --- 1. PENGATURAN UTAMA BOT ---
-    tokenUser: '-',
-    tokenAdmin: '-:',
-    ownerId: '-',
+    tokenUser: '8659534158:AAHy9n_YBDLwyCts9wFHB6ViWTwCPAX7w8Q',
+    tokenAdmin: '8659534158:AAHy9n_YBDLwyCts9wFHB6ViWTwCPAX7w8Q',
+    ownerId: '1957639198',
 
 
     // --- 3. PENGATURAN API KEY LAYANAN ---
-    rumahOtpApiKey: '',
+    rumahOtpApiKey: 'rk-dev-UfG51tOBYDwYMZpmEmlgQjjqGNTjzbxo',
 
     // --- 4. PENGATURAN PEMBAYARAN PAKASIR ---
-    pakasirProject: 'your-project-slug',   // Slug proyek di app.pakasir.com
-    pakasirApiKey: 'your-pakasir-api-key', // API Key proyek di app.pakasir.com
+    pakasirProject: 'scbackend',   // Slug proyek di app.pakasir.com
+    pakasirApiKey: 'Q8gFjH9TocgWARqc8b7kg1OBlrUDlWj9', // API Key proyek di app.pakasir.com
 
     // --- 5. INFO BOT & LINK ---
     botUsername: '@',
